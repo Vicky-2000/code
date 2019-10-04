7a83127062526eb5473f19075f2f24f11efe6164
Vicky-2000 <56164489+Vicky-2000@users.noreply.github.com>
2019-10-04 23:50:18 +0530

    Update reverse array.c

--- reverse array.c
+++ reverse array.c
@@ -2,6 +2,7 @@
 int main()
 {int a[1000],i,n,temp;
 scanf("%d",&n);
+ printf("enter the range");
 for(i=0;i<n;i++)
 {scanf("%d",&a[i]);
 }
@@ -10,7 +11,7 @@ for(i=0;i<n/2;i++)
 temp=a[i];
 a[i]=a[n-1-i];
 a[n-1-i]=temp;
-}
+}printf("reverse array is");
 for(i=0;i<n;i++)
 {printf("%d ",a[i]);
 }
