fe89e94a18a75b9c13cae86d66f4289e837223ad
Dibyajeet3110 <54031838+Dibyajeet3110@users.noreply.github.com>
2019-10-04 21:08:44 +0530

    Update merging.c

--- merging.c
+++ merging.c
@@ -5,15 +5,9 @@ scanf("%d%d",&n1,&n2);
 for(i=0;i<n1;i++)
 {scanf("%d",&a[i]);
 }
-for(i=0;i<n1;i++)
-{printf("%d ",a[i]);
-}printf("\n");
 for(i=0;i<n2;i++)
 {scanf("%d",&b[i]);
 }
-for(i=0;i<n2;i++)
-{printf("%d ",b[i]);
-}printf("\n");
 i=0;
 j=0;
 k=0;
